f8b7a62b21c97d6f37fed5bfb3a5e514acd8da00
cyanide126 <cyada2000@gmail.com>
2021-01-25 12:00:32 +0700

    lab6

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -21,10 +21,35 @@ function addProjectDetails(e) {
 	// Prevent following the link
 	e.preventDefault();
 
+
 	// Get the div ID, e.g., "project3"
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get("/project/"+idNumber, callback)
+	console.log("/project/" + idNumber);
+
+
+}
+
+function callback(result){
+	console.log(result)
+
+	var projecthtml = '<p><small>'+ result.date+'</small></p>' +
+	'<img src="' + result.image + '" class="detailsImage">' + result.summary;
+	$("#project"+result.id+" .details").html(projecthtml)
+
+
+	//var nowID = '#' + result.projectID;
+	//$(nowID).html('foo');
+
+	//var projectHTML = '<img src="' + result.image +
+	//	'" class="detailsImage">' +
+   // 	'<p>' + result.title + '</p>' +
+   // 	'<p><small>' + result.date + '</small></p>' + 
+   // 	result.summary; 
+	//$("#project" + result.id + " .details").html(projectHTML);
 }
